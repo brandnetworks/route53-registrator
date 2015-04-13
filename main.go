307d13c707bb7d1b3eb1395e2e5dadcd9bd0e3a1
Rob Johnson <rob@rob-johnson.me>
2015-04-13 13:22:49 +0100

    clean up logging to use glog

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -13,6 +11,7 @@ import (
 	"github.com/awslabs/aws-sdk-go/aws/awsutil"
 	"github.com/awslabs/aws-sdk-go/service/route53"
 	dockerapi "github.com/fsouza/go-dockerclient"
+	"github.com/golang/glog"
 )
 
 func getopt(name, def string) string {
@@ -24,7 +23,7 @@ func getopt(name, def string) string {
 
 func assert(err error) {
 	if err != nil {
-		log.Fatal(err)
+		glog.Error(err)
 	}
 }
 
@@ -42,7 +41,7 @@ func hostname(metadataServerAddress string) (hostname string) {
 	return string(body)
 }
 
-//container names start with a /
+//container names start with a /. This function removes the leading / if it exists.
 func normalizedContainerName(original string) (normalized string) {
 	if strings.HasPrefix("/", original) {
 		return original
@@ -50,23 +49,25 @@ func normalizedContainerName(original string) (normalized string) {
 	return strings.Join([]string{"/", original}, "")
 }
 
+//Given a container ID and a name, assert whether the name of the container matches that of the provided name.
 func isObservedContainer(client *dockerapi.Client, containerId string, targetContainerName string) (observed bool) {
 	container, err := client.InspectContainer(containerId)
 	assert(err)
 	if container.Name == targetContainerName {
 		return true
 	}
-	log.Println("no match")
+	glog.Infof("Container ", containerId, " did not match name", targetContainerName)
 	return false
 }
 
+//Find all resource records in a AWS Hosted Zone that match a given name.
 func findMatchingResourceRecords(client *route53.Route53, zone string, setName string) (recordSet []*route53.ResourceRecordSet, err error) {
 	resources, err := client.ListResourceRecordSets(&route53.ListResourceRecordSetsInput{
 		HostedZoneID: aws.String(zone),
 	})
 	if awserr := aws.Error(err); awserr != nil {
 		// A service error occurred.
-		fmt.Println("Error:", awserr.Code, awserr.Message)
+		glog.Errorf("AWS Error: \n Code: %s \n Message: %s", awserr.Code, awserr.Message)
 		return nil, err
 	} else if err != nil {
 		// A non-service error occurred.
@@ -77,14 +78,14 @@ func findMatchingResourceRecords(client *route53.Route53, zone string, setName s
 		//FQDNs have a trailing dot. Check if that which has been provided
 		//matches the route, irrespective of the trailing dot
 		if strings.TrimRight(*route.Name, ".") == strings.TrimRight(setName, ".") {
-			fmt.Println("Found existing ResourceRecord Set with name ", strings.TrimRight(setName, "."))
-			fmt.Println(awsutil.StringValue(route))
+			glog.Infof("Found existing ResourceRecord Set with name ", strings.TrimRight(setName, "."))
 			matching = append(matching, route)
 		}
 	}
 	return matching, nil
 }
 
+//Given a CNAME and a value, create a AWS Resource Record Set with CNAME -> Value.
 func createResourceRecordSet(cname string, value string) (resourceRecordSet *route53.ResourceRecordSet) {
 	return &route53.ResourceRecordSet{
 		Name: aws.String(cname),
@@ -121,12 +122,13 @@ func main() {
 	var zoneId = flag.String("zone", "Z1P7DHMHEAX6O3", "The route53 hosted zone id")
 	var cname = flag.String("cname", "my-test-registry.realtime.bnservers.com", "The CNAME for the record set")
 
+	//Print some debug information
 	flag.Parse()
-	log.Println(*region)
-	log.Println(*metadataIP)
-	log.Println(*cname)
-	log.Println(*zoneId)
-	log.Println(*containerName)
+	glog.Info(*region)
+	glog.Info(*metadataIP)
+	glog.Info(*cname)
+	glog.Info(*zoneId)
+	glog.Info(*containerName)
 
 	docker, err := dockerapi.NewClient(getopt("DOCKER_HOST", "unix:///tmp/docker.sock"))
 	assert(err)
@@ -143,9 +145,9 @@ func main() {
 
 	matchingResourceRecords, err := findMatchingResourceRecords(client, *zoneId, *cname)
 	if err != nil {
-		log.Fatalf("Failed:", err)
+		glog.Errorf("Error searching for exisiting records:", err)
 	}
-	fmt.Printf("Found %d existing records with a matching name. Destroying. \n", len(matchingResourceRecords))
+	glog.Infof("Found %d existing records with a matching name. Destroying. \n", len(matchingResourceRecords))
 	if matchingResourceRecords != nil {
 		var changes []*route53.Change
 		for _, set := range matchingResourceRecords {
@@ -162,49 +164,50 @@ func main() {
 		}
 		resp, err := client.ChangeResourceRecordSets(params)
 		if awserr := aws.Error(err); awserr != nil {
-			fmt.Println("Error:", awserr.Code, awserr.Message)
+			glog.Errorf("AWS Error: \n Code: %s \n Message: %s", awserr.Code, awserr.Message)
 		} else if err != nil {
-			fmt.Println("Error removing existing records: ", err)
+			glog.Errorf("Error removing existing records: ", err)
 			panic(err)
 		}
-		fmt.Println(awsutil.StringValue(resp))
+		glog.Infof("Response from removing existing records: \n %+v", awsutil.StringValue(resp))
 	}
 
-	log.Println("Listening for Docker events ...")
+	glog.Infof("Listening for Docker events ...")
 
 	// Process Docker events
 	for msg := range events {
 		switch msg.Status {
 		case "start":
-			log.Println("docker start")
+			glog.Infof("Event: container %s died", msg.ID)
 			if isObservedContainer(docker, msg.ID, targetContainer) {
 				resp, err := resourceRecordSetRequest(client, "CREATE", *zoneId, *cname, hostname(*metadataIP))
 				if awserr := aws.Error(err); awserr != nil {
-					fmt.Println("Error:", awserr.Code, awserr.Message)
+					glog.Errorf("AWS Error: \n Code: %s \n Message: %s", awserr.Code, awserr.Message)
 				} else if err != nil {
 					// A non-service error occurred.
+					glog.Errorf("Error occured creating records: \n %s", err)
 					panic(err)
 				}
-				fmt.Println(awsutil.StringValue(resp))
+				glog.Infof("Response: \n: %s", awsutil.StringValue(resp))
 			}
 		case "die":
-			log.Println("docker die")
+			glog.Infof("Event: container %s died.", msg.ID)
 			if isObservedContainer(docker, msg.ID, targetContainer) {
 				resp, err := resourceRecordSetRequest(client, "DELETE", *zoneId, *cname, hostname(*metadataIP))
 				if awserr := aws.Error(err); awserr != nil {
-					fmt.Println("Error:", awserr.Code, awserr.Message)
+					glog.Errorf("AWS Error: \n Code: %s \n Message: %s", awserr.Code, awserr.Message)
 				} else if err != nil {
 					// A non-service error occurred.
+					glog.Errorf("Error occured creating records: \n %s", err)
 					panic(err)
 				}
-				fmt.Println(awsutil.StringValue(resp))
+				glog.Infof("Response: \n: %s", awsutil.StringValue(resp))
 			}
 		case "default":
-			log.Println(msg)
+			glog.Infof("Event: container %s ignoring", msg.ID)
 		}
 	}
 
 	quit := make(chan struct{})
 	close(quit)
-	log.Println("bye")
 }
